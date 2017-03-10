9cd2acc2bf220c6ae25034058762c11ec022687c
forecho <caizhenghai@gmail.com>
2017-03-10 09:16:32 +0000

    use weui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+//使用 commonJS 模块方式 *可能需要webpack插件
+//import { Button } from 'react-weui'
+
+//使用 ES6 方式引入
+import WeUI from 'react-weui';
+
+//引入样式库
+import 'weui';
+import 'react-weui/lib/react-weui.min.css';
+
+const {Button} = WeUI;
+
+class App extends Component {
+    render() {
+        return (
+            <Button>hello wechat</Button>
+        );
+    }
+}
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('container')
 );
