ddb993ca8091097ff01d74a9e4b08fe99af376f0
forecho <caizhenghai@gmail.com>
2017-03-20 07:23:40 +0000

    教程：事件监听

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    onMakeMe() {
+        console.log(this.age);
+        this.age += 3;
+    }
+
     render() {
         return (
             <div>
                 <p> In a new Component!</p>
                 <p>我的名字叫 {this.props.user.name}，{this.props.children}</p>
-                <p>你的名字叫 {this.props.name}</p>
+                <p>你的名字叫 {this.props.name}，今年{this.props.age}岁了</p>
+                <button onClick={() => this.onMakeMe()} className="btn">点我试试看1</button>
+                <button onClick={this.onMakeMe.bind(this)} className="btn">点我试试看2</button>
             </div>
         );
     }
