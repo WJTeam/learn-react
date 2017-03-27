e21576f0ab7a8b7718723a4de834d8d4404064c4
forecho <caizhenghai@gmail.com>
2017-03-27 09:10:55 +0000

    教程：路由的基本使用

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,83 +1,11 @@
 import React from "react";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            homeLink: props.initialLinkName
-        };
-    }
-
-    onMakeMe() {
-        this.setState({
-            age: this.state.age + 3
-        });
-    }
-
-    componentWillMount() {
-        console.log("组件将挂载");
-    }
-
-    componentDidMount() {
-        console.log("组件已经挂载");
-    }
-
-    componentWillReceiveProps(nextProps) {
-        console.log("组件将要接受参数", nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("组件应该更新吗？", nextProps, nextState);
-        return true;
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        console.log("组件将要更新", nextProps, nextState);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("组件已经更新", prevProps, prevState);
-
-    }
-
-    componentWillUnmount() {
-        console.log("组件将要取消挂载");
-    }
-
-    onChangeLink() {
-        this.props.changeLink(this.state.homeLink)
-    }
-
-    onHandleChangeLink(event) {
-        this.setState({
-            homeLink: event.target.value
-        });
-    }
-
     render() {
         return (
             <div>
-                <p> In a new Component!</p>
-                <p>我的名字叫 {this.props.user.name}，{this.props.children}</p>
-                <p>你的名字叫 {this.props.name}，今年{this.state.age}岁了</p>
-                <button onClick={() => this.onMakeMe()} className="btn">点我试试看1</button>
-                <hr/>
-                <button onClick={this.onMakeMe.bind(this)} className="btn">点我试试看2</button>
-                <hr/>
-                <button onClick={this.props.hello} className="btn">打招呼</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} onChange={this.onHandleChangeLink.bind(this)}/>
-                <button onClick={this.onChangeLink.bind(this)} className="btn">点我改变菜单</button>
+                <h2>Home</h2>
             </div>
         );
     }
 }
-
-// 规范参数类型
-Home.propTypes = {
-    name: React.PropTypes.string,
-    user: React.PropTypes.object,
-    children: React.PropTypes.element.isRequired, // React 元素 且必须
-    hello: React.PropTypes.func // 函数式元素
-};
--- src/index.js
+++ src/index.js
@@ -1,61 +1,29 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
-import {Header} from "./components/Header";
 import {Home} from "./components/Home";
+import {User} from "./components/User";
+import {Topics} from "./components/Topics";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            homeLink: "首页",
-            homeMounted: true
-        };
-    }
-
-    onSayHello() {
-        alert("hello");
-    };
-
-    onChangeLinkName(newName) {
-        this.setState({
-            homeLink: newName
-        });
-    }
-
-    onChangeHomeMounted() {
-        this.setState({
-            homeMounted: !this.state.homeMounted
-        });
-    }
-
     render() {
-        let user = {
-            name: "forecho"
-        };
-
-        let homeComponent = "";
-        if (this.state.homeMounted) {
-            homeComponent = (
-                <Home user={user}
-                      name={"echo"}
-                      initialAge={18}
-                      hello={this.onSayHello}
-                      initialLinkName={this.state.homeLink}
-                      changeLink={this.onChangeLinkName.bind(this)}>
-                    <span>我今年18岁了</span>
-                </Home>
-            );
-        }
         return (
-            <div className="container">
-                <Header homeLink={this.state.homeLink}/>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/user">About</Link></li>
+                        <li><Link to="/topics">Topics</Link></li>
+                    </ul>
 
-                {homeComponent}
+                    <hr/>
 
-                <button onClick={this.onChangeHomeMounted.bind(this)} className="btn">点我显示隐藏</button>
-
-            </div>
+                    <Route path="/" component={Home}/>
+                    <Route path="/user" component={User}/>
+                    <Route path="/topics" component={Topics}/>
+                </div>
+            </Router>
         );
     }
 }
