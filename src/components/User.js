c731a4ba2f3de77b63c0d5d5e994fef4e428810f
forecho <caizhenghai@gmail.com>
2017-06-07 17:29:07 +0800

    添加用户组件

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class User extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>用户页面</h3>
+                <p>用户 ID：</p>
+            </div>
+        );
+    };
+}
